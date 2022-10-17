ce048e8360384afebbada906ca54007801f3a4de
Ruby <ruby0322@ntu.im>
2022-10-17 14:31:11 +0800

    .

--- client/src/components/sections/Hero.js
+++ client/src/components/sections/Hero.js
@@ -60,7 +60,7 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              想不到<span className="text-color-primary">名字</span>
+              我s笨蛋<span className="text-color-primary">名字</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
